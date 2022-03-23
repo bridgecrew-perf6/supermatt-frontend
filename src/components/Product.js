433784e0460469a03a14dbe5119a1fbdb15ecf8a
deepeters <dennis95peters@gmail.com>
2022-03-23 05:05:27 +0300

    update api to backend on heroku

--- src/components/Product.js
+++ src/components/Product.js
@@ -121,7 +121,7 @@ class Product extends React.Component {
         <Card className="border border-dark bg-dark text-white">
           <Card.Header>
             <FontAwesomeIcon icon={this.state.id ? faEdit : faPlusSquare} />
-            {this.state.id ? "Update Product" : "Add Product"}
+            {this.props.match.params.id ? "Update Product" : "Add Product"}
           </Card.Header>
           <Form
             onReset={this.resetProduct}
@@ -188,7 +188,7 @@ class Product extends React.Component {
             </Card.Body>
             <Card.Footer style={{ textAlign: "right" }}>
               <Button size="sm" variant="success" type="submit">
-                <FontAwesomeIcon icon={faSave} /> {this.state.id ? "Update" : "Save"}
+                <FontAwesomeIcon icon={faSave} /> {this.props.match.params.id ? "Update" : "Save"}
               </Button>{" "}
               <Button size="sm" variant="info" type="reset">
                 <FontAwesomeIcon icon={faUndo} />
